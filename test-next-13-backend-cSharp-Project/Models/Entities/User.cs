a528fc9ff883d6a7c8352174e8af701eb86f9bc6
wongprom <jimmybackstrom@hotmail.com>
2025-05-18 14:16:16 +0200

    feat(models): align User entity with Supabase, verified Nitro query
    • Added [Column] attributes so EF/Nitro generate lowercase identifiers
    • Marked properties as `required`
    • Confirmed end-to-end User read/write works via Nitro data service

--- test-next-13-backend-cSharp-Project/Models/Entities/User.cs
+++ test-next-13-backend-cSharp-Project/Models/Entities/User.cs
@@ -1,9 +1,18 @@
-namespace test_next_13_backend_cSharp_Project.Models.Entities;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace test_next_13_backend_cSharp_Project.Models.Entities;
+// User entity for Supabase: lowercase [Column] names, all properties required.
 public class User
 {
-   public int Id { get; set; }  // Supabase uses bigint, this maps fine
+    [Column("id")]
+    public required int Id { get; set; }
+
+    [Column("name")]
     public required string Name { get; set; }
+
+    [Column("email")]
     public required string Email { get; set; }
+
+    [Column("password")]
     public required string Password { get; set; }
 }
